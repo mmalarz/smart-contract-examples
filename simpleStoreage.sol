4537c5e82de6490c654ecffca85f6644d04e3601
mmalarz <mmalarz@protonmail.com>
2018-08-13 05:41:51 +0100

    Fix compilation warnings

--- simpleStoreage.sol
+++ simpleStoreage.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.0;
 contract Storage {
     uint storedData;
 
-    function setStoredData(uint _storedData) {
+    function setStoredData(uint _storedData) public {
         storedData = _storedData;
     }
 
-    function getStoredData() returns (uint) {
+    function getStoredData() public view returns (uint) {
         return storedData;
     }
 }
